34653d8bb80787fed70ca8f2d8cc4057fe179bf2
Unitech <strzelewicz.alexandre@gmail.com>
2018-05-15 18:06:09 +0200

    refresh module

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 
-process.env.MODULE_DEBUG = (process.NODE_ENV == 'production' ? false : true);
-
 var pmx     = require('pmx');
 var pm2     = require('pm2');
 var async   = require('async');
 var pkg     = require('./package.json');
-var debug   = require('debug')(pkg.name);
-
-var conf    = pmx.initModule();
 
 var Probe = pmx.probe();
 
 var app_updated = Probe.counter({
-  name : 'App updated'
+  name : 'Updates'
 });
 
 function autoPull(cb) {
   pm2.list(function(err, procs) {
+    if (err) return console.error(err);
 
     async.forEachLimit(procs, 1, function(proc, next) {
       if (proc.pm2_env && proc.pm2_env.versioning) {
-        debug('pull And Reload %s', proc.name);
         pm2.pullAndReload(proc.name, function(err, meta) {
           if (meta) {
             var rev = meta.rev;
@@ -39,7 +34,7 @@ function autoPull(cb) {
             }
           }
           if (err)
-            debug('App %s already at latest version', proc.name);
+            console.log('App %s already at latest version', proc.name);
           return next();
         });
       }
@@ -49,18 +44,41 @@ function autoPull(cb) {
   });
 }
 
-pm2.connect(function() {
-  console.log('pm2-auto-pull module connected to pm2');
+pmx.initModule({
+  widget : {
+    type             : 'generic',
+    theme            : ['#111111', '#1B2228', '#807C7C', '#807C7C'],
 
-  var running = false;
+    el : {
+      probes  : true,
+      actions : true
+    },
 
-  setInterval(function() {
-    if (running == true) return false;
+    block : {
+      actions : true,
+      issues  : true,
+      meta : true,
+      cpu: true,
+      mem: true
+    }
 
-    running = true;
-    autoPull(function() {
-      running = false;
-    });
-  }, conf.interval || 30000);
+    // Status
+    // Green / Yellow / Red
+  }
+}, function(err, conf) {
+  pm2.connect(function() {
+    console.log('pm2-auto-pull module connected to pm2');
 
+    var running = false;
+
+    setInterval(function() {
+      if (running == true) return false;
+
+      running = true;
+      autoPull(function() {
+        running = false;
+      });
+    }, conf.interval || 30000);
+
+  });
 });
